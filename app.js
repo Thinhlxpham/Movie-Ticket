227b2b4eaf4ad847e393a976ec6e5058901a0040
Thinh Pham <thinhpham1842005@gmail.com>
2025-03-09 20:35:42 -0400

    help me fix JavaScript and HTML file

--- app.js
+++ app.js
@@ -1,72 +1,76 @@
-async function fetchData(){
-  const apiKey = await fetch('http://www.omdbapi.com/?i=tt3896198&apikey=b3770ca8')
 
-  const data = await apiKey.json()
 
-  return data
+// 'http://www.omdbapi.com/?i=tt3896198&apikey=b3770ca8'
+
+async function fetchData(url) {
+  try {
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
- async function searchBarEvent(){
-  let search = await fetchData()
-  const movieList = document.querySelector('.movies_content');
+async function moviesSearch(value) {
+  if (!value.trim()) return;
 
-  const searchHTML = search.map((search) => {
-    return `<div class="movies_list">
-            <i class="fa-solid fa-spinner movie_list--loading"></i>
-            <div class="movie">
-              <figure class="movie_img--wrapper">
-                <img class="movie_img" src="asset/postertest.jpg" alt="">
-                <h3 class="movie_info--title">Guardians of the Galaxy Vol .2</h3>
+  let search = await fetchData(`http://www.omdbapi.com/?i=tt3896198&apikey=b3770ca8`);
+  if (!search || !search.Search) {
+    console.log("No movies found!");
+    return;
+  }
 
-              </figure>
-              <h4 class="movie_title">Guardians of the Galaxy Vol. 2</h4>
-            </div>
-            <div class="movie">
-              <figure class="movie_img--wrapper">
-                <img class="movie_img" src="asset/postertest2.jpg" alt="">
-                <h3 class="movie_info--title">The Avenger</h3>
+  const array = search.Search.slice(0, 6);
+  const movies = document.querySelector(".movies_list");
 
-              </figure>
-              <h4 class="movie_title">The Avenger</h4>
-            </div>
-            <div class="movie">
-              <figure class="movie_img--wrapper">
-                <img class="movie_img" src="asset/postertest3.jpg" alt="">
-                <h3 class="movie_info--title">Spider-man: Homecoming</h3>
+  const moviesHTML = array
+    .map(
+      (movie) => `
+      <div class="movie">
+        <figure class="movie_img--wrapper">
+          <img class="movie_img" src="${movie.Poster}" alt="">
+          <h3 class="movie_info--title">${movie.Title}</h3>
+        </figure>
+        <h4 class="movie_title">${movie.Title}</h4>
+      </div>`
+    )
+    .join("");
 
-              </figure>
-              <h4 class="movie_title">Spider-man: Homecoming</h4>
-            </div>
-            <div class="movie">
-              <figure class="movie_img--wrapper">
-                <img class="movie_img" src="asset/postertest4.jpg" alt="">
-                <h3 class="movie_info--title">Minions: Rise of Gru</h3>
-
-              </figure>
-              <h4 class="movie_title">Minions: Rise of Gru</h4>
-            </div>
-            <div class="movie">
-              <figure class="movie_img--wrapper">
-                <img class="movie_img" src="asset/postertest5.jpg" alt="">
-                <h3 class="movie_info--title">Spider-Man: Into the Spider-Verse</h3>
-
-              </figure>
-              <h4 class="movie_title">Spider-Man: Into the Spider-Verse</h4>
-            </div>
-            <div class="movie">
-              <figure class="movie_img--wrapper">
-                <img class="movie_img" src="asset/postertest6.jpg" alt="">
-                <h3 class="movie_info--title">Nope</h3>
-
-              </figure>
-              <h4 class="movie_title">Nope</h4>
-            </div>
-          </div>`;
-  });
-
-  movieList.innerHTML = searchHTML;
+  movies.innerHTML = moviesHTML;
 }
-searchBarEvent();
 
+function searchActive() {
+  document.querySelector(".nav_input").focus();
+}
 
+function searchResult(value) {
+  const searchBar = document.querySelector(".movies_top");
+  searchBar.innerHTML = `
+    <h2 class="movies_top--title">Search results for:</h2>
+    <h2 class="movie_search--result">"${value}"</h2>
+  `;
+}
 
+async function searchBarEnter(event) {
+  if (event.keyCode !== 13) return;
+
+  let value = document.querySelector(".nav_input").value.trim();
+  if (!value) return;
+
+  searchResult(value);
+  await moviesSearch(value);
+}
+
+async function searchOnClick() {
+  let value = document.querySelector(".movie_input").value.trim();
+  if (!value) return;
+
+  searchResult(value);
+  await moviesSearch(value);
+}
+
+console.log(moviesSearch('The Avenger'));
